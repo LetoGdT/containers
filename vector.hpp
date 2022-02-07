7531dadce6f7bc44a1c2babad10f2f10f4c39878
Léto Gaudet dit Trafit <lgaudet-@student.42lyon.fr>
2022-02-07 20:01:35 +0100

    k

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: lgaudet- <lgaudet-@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 14:25:09 by lgaudet-          #+#    #+#             */
-/*   Updated: 2022/02/03 18:39:14 by lgaudet-         ###   ########lyon.fr   */
+/*   Updated: 2022/02/07 20:01:24 by lgaudet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,33 +48,118 @@ namespace ft {
 			friend class ReverseVectorIter<T>;
 			friend class ConstReverseVectorIter<T>;
 
-			vector();
-			explicit vector(const Allocator& alloc);
+			vector(): vector(Allocator()) {}
+			explicit vector(const Allocator& alloc) {
+				_alloc = alloc;
+				_capacity = 10;
+				_size = 0;
+				_data = _alloc.allocate(_capacity);
+			}
+
 			explicit vector(size_type count, 
 							const T& value = T(),
-							const Allocator& alloc = Allocator());
+							const Allocator& alloc = Allocator()) {
+				_alloc = alloc;
+				_capacity = count;
+				_size = count;
+				_data = _alloc.allocate(count);
+				for (size_type i = 0 ; i < count ; i++)
+					_data[i] = value;
+			}
+
 			template<class InputIt>
 				vector(InputIt first,
 						InputIt last,
-						const Allocator& alloc = Allocator());
-			vector(const vector& other);
-			~vector();
+						const Allocator& alloc = Allocator()) {
+				_alloc = alloc;
+				size_type count = 0;
+				for (InputIt it = first ; it != last ; it++)
+					count++;
+				_capacity = count;
+				_size = count;
+				_data = _alloc.allocate(count);
+				size_type i = 0;
+				for (InputIt it = first ; it != last ; it++, i++)
+					_data[i] = *it;
+			}
 
-			vector& operator=(const vector& other);
-			void assign( size_type count, const T& value );
+			vector(const vector& other) {
+				this->_alloc = other.alloc;
+				this->_capacity = other._capacity;
+				this->_size = other._size;
+				this->_data = _alloc.allocate(_capacity);
+				for (size_type i = 0 ; i < _size ; i++)
+					this->_data[i] = other._data[i];
+			}
+			~vector() {
+				_alloc.deallocate(_data, _capacity);
+			}
+
+			vector& operator=(const vector& other) {
+				_alloc.deallocate(_data, _capacity);
+				_size = other._size;
+				_capacity = other._capacity;
+				_data = _alloc.allocate(_capacity);
+				for (size_type i = 0 ; i < _size ; i++)
+					_data[i] = other._data[i];
+			}
+
+			void assign( size_type count, const T& value ) {
+				_alloc.deallocate(_data, _capacity);
+				_size = count;
+				_capacity = count;
+				_data = _alloc.allocate(count);
+				for (size_type i = 0 ; i < count ; i++)
+					_data[i] = value;
+			}
+
 			template<class InputIt>
 				void assign(InputIt first,
-							InputIt last);
-			allocator_type get_allocator() const;
+							InputIt last) {
+				size_type count = 0;
+				for (InputIt it = first ; it != last ; it++)
+					count++;
+				_capacity = count;
+				_size = count;
+				_data = _alloc.allocate(count);
+				size_type i = 0;
+				for (InputIt it = first ; it != last ; it++, i++)
+					_data[i] = *it;
+			}
 
-			reference at(size_type pos);
-			const_reference at(size_type pos) const;
-			reference front();
-			const_reference front() const;
-			reference back();
-			const_reference back() const;
-			T* data();
-			const T* data() const;
+			allocator_type get_allocator() const {
+				return const_cast<allocator_type>(_alloc);
+			}
+
+			reference at(size_type pos) {
+				if (pos >= _size)
+					throw std::out_of_range("");
+				return _data[pos];
+			}
+			const_reference at(size_type pos) const {
+				if (pos >= _size)
+					throw std::out_of_range("");
+				return const_cast<const_reference>(_data[pos]);
+			}
+
+			reference front() {
+				return _data[0];
+			}
+			const_reference front() const {
+				return const_cast<const_reference>(_data[0]);
+			}
+			reference back() {
+				return _data[_size - 1];
+			}
+			const_reference back() const {
+				return const_cast<const_reference>(_data[_size - 1]);
+			}
+			T* data() {
+				return _data;
+			}
+			const T* data() const {
+				return const_cast<const_reference>(_data);
+			}
 
 			iterator begin() { return VectorIter<T>(*this, 0); }
 			iterator end() { return VectorIter<T>(*this, _size); }
@@ -85,11 +170,19 @@ namespace ft {
 			const_reverse_iterator rbegin() const { return ConstReverseVectorIter<T>(*this, 0); }
 			const_reverse_iterator rend() const { return ConstReverseVectorIter<T>(*this, _size); }
 
-			bool empty() const;
-			size_type size() const;
-			size_type max_size() const;
+			bool empty() const {
+				return _size == 0;
+			}
+			size_type size() const {
+				return _size;
+			}
+			size_type max_size() const {
+				return std::numeric_limits<size_type>::max();
+			}
 			void reserve( size_type new_cap );
-			size_type capacity() const;
+			size_type capacity() const {
+				return _capacity;
+			}
 
 			void clear();
 			iterator insert(iterator pos, const T& value);
@@ -125,6 +218,7 @@ namespace ft {
 			T* _data;
 			size_type _size;
 			size_type _capacity;
+			Allocator& _alloc;
 	};
 
 	template<typename T> class VectorIter {
